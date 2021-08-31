dde87cd54528f2dc618c3ef99e698c52bb520473
Martin Mwenda <43547404+MartinMugambi@users.noreply.github.com>
2021-08-31 03:58:20 +0000

    Update books.js

--- routes/books.js
+++ routes/books.js
@@ -2,32 +2,31 @@ var express = require('express');
 var router = express.Router();
 var dbConn  = require('../lib/db');
  
-// display books page
 router.get('/', function(req, res, next) {
       
     dbConn.query('SELECT * FROM books ORDER BY id desc',function(err,rows)     {
  
         if(err) {
             req.flash('error', err);
-            // render to views/books/index.ejs
+           
             res.render('books',{data:''});   
         } else {
-            // render to views/books/index.ejs
+          
             res.render('books',{data:rows});
         }
     });
 });
 
-// display add book page
+
 router.get('/add', function(req, res, next) {    
-    // render to add.ejs
+   
     res.render('books/add', {
         name: '',
         author: ''        
     })
 })
 
-// add a new book
+
 router.post('/add', function(req, res, next) {    
 
     let name = req.body.name;
@@ -37,16 +36,16 @@ router.post('/add', function(req, res, next) {
     if(name.length === 0 || author.length === 0) {
         errors = true;
 
-        // set flash message
+       
         req.flash('error', "Please enter name and author");
-        // render to add.ejs with flash message
+        
         res.render('books/add', {
             name: name,
             author: author
         })
     }
 
-    // if no error
+    
     if(!errors) {
 
         var form_data = {
@@ -54,13 +53,13 @@ router.post('/add', function(req, res, next) {
             author: author
         }
         
-        // insert query
+        
         dbConn.query('INSERT INTO books SET ?', form_data, function(err, result) {
-            //if(err) throw err
+           
             if (err) {
                 req.flash('error', err)
                  
-                // render to add.ejs
+                
                 res.render('books/add', {
                     name: form_data.name,
                     author: form_data.author                    
@@ -73,7 +72,7 @@ router.post('/add', function(req, res, next) {
     }
 })
 
-// display edit book page
+
 router.get('/edit/(:id)', function(req, res, next) {
 
     let id = req.params.id;
@@ -81,14 +80,14 @@ router.get('/edit/(:id)', function(req, res, next) {
     dbConn.query('SELECT * FROM books WHERE id = ' + id, function(err, rows, fields) {
         if(err) throw err
          
-        // if user not found
+        
         if (rows.length <= 0) {
             req.flash('error', 'Book not found with id = ' + id)
             res.redirect('/books')
         }
-        // if book found
+        
         else {
-            // render to edit.ejs
+            
             res.render('books/edit', {
                 title: 'Edit Book', 
                 id: rows[0].id,
@@ -112,7 +111,7 @@ router.post('/update/:id', function(req, res, next) {
         
         // set flash message
         req.flash('error', "Please enter name and author");
-        // render to add.ejs with flash message
+        
         res.render('books/edit', {
             id: req.params.id,
             name: name,
@@ -120,7 +119,7 @@ router.post('/update/:id', function(req, res, next) {
         })
     }
 
-    // if no error
+  
     if( !errors ) {   
  
         var form_data = {
@@ -129,11 +128,11 @@ router.post('/update/:id', function(req, res, next) {
         }
         // update query
         dbConn.query('UPDATE books SET ? WHERE id = ' + id, form_data, function(err, result) {
-            //if(err) throw err
+           
             if (err) {
-                // set flash message
+                
                 req.flash('error', err)
-                // render to edit.ejs
+               
                 res.render('books/edit', {
                     id: req.params.id,
                     name: form_data.name,
@@ -153,16 +152,16 @@ router.get('/delete/(:id)', function(req, res, next) {
     let id = req.params.id;
      
     dbConn.query('DELETE FROM books WHERE id = ' + id, function(err, result) {
-        //if(err) throw err
+        
         if (err) {
-            // set flash message
+           
             req.flash('error', err)
-            // redirect to books page
+            
             res.redirect('/books')
         } else {
-            // set flash message
+           
             req.flash('success', 'Book successfully deleted! ID = ' + id)
-            // redirect to books page
+           
             res.redirect('/books')
         }
     })
